739cf774c6f9f32dc59243faaa8b4bc86cdf8852
Nick Weiner <nicholas.weiner@icloud.com>
2021-12-07 20:25:45 -0500

    inRange method

--- _.js
+++ _.js
@@ -3,8 +3,21 @@ const _ = {
         let lowerClampedValue = Math.max(number, lower);
         let clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
+    },
+    inRange(number, start, end) {
+        if (end === undefined) {
+            end = start
+            start = 0
+        } if (start > end) {
+            var temp = end;
+            end = start;
+            start = temp;
+        } 
+        var isInRange = start <= number && number < end
+        return isInRange;
     }
-}
+
+};
 
 
 
