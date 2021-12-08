79aaf88073e9f29af4ce6143ab5bd0bf77b212c5
Nick Weiner <nicholas.weiner@icloud.com>
2021-12-08 12:11:04 -0500

    chunks method

--- _.js
+++ _.js
@@ -61,7 +61,22 @@ const _ = {
         let droppedArray = array.slice(n, array.length);
         return droppedArray;
     },
-    
+    dropWhile(array, predicate){
+        const cb = (element, index) => {
+            return !predicate(element, index, array);
+        };
+        let dropNumber = array.findIndex(cb);
+        let droppedArray = this.drop(array, dropNumber);
+        return droppedArray;
+    },
+    chunk(array, size=1){
+        let arrayChunks = [];
+        for(let i = 0; i < array.length; i+= size){
+            let arrayChunk = array.slice(i, i+size);
+            arrayChunks.push(arrayChunk);
+        }
+        return arrayChunks
+    }
 
 
 
